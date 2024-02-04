3ba51a6000ad3ec3f1bbb6cc2c11cf12cfce120a
TejasJore33 <tejasjore33@Gmail.com>
2024-02-04 09:57:10 +0530

    react basic
    react basic

---
+++ index.js
@@ -0,0 +1,16 @@
+// const reactHeading = React.createElement("h1", { className: "head", id: "reactHead", children: "Hello React!" });
+// // console.log("React Heading", reactHeading);
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
+
+
+// React with jsx
+
+
+const jsxHeading =
+<>
+<h1 className="head">Hello Jsx!</h1>
+<p>Hey this is the paragraph of the jsx....</p>
+</> 
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+
---
+++ react.js
@@ -0,0 +1,3 @@
+const reactHeading = React.createElement("img", { className: "image", src: "https://files.codingninjas.in/coding-ninjas-24647.png" });
+ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
